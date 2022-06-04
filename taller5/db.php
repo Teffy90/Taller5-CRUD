634f5ffe4c3564fc5bdac558fe74c777903d1878
Teffy90 <eocampol@unal.edu.co>
2022-06-03 21:18:09 -0500

    Se hacen correcciones generales

--- taller5/db.php
+++ taller5/db.php
@@ -6,7 +6,21 @@ $conn = mysqli_connect(
     'localhost',
     'root',
     '',
-    'catalogopeliculas',
+    'catalogopeliculas'
 );
+
+/* if($conn){
+    echo 'si';
+}    
+else {
+    echo 'no';
+}
+
+$query = $conn -> query ("SELECT * FROM genero");
+                    
+while ($valores = mysqli_fetch_array($query)) {
+                        
+  echo '<option value="'.$valores['id_genero'].'">'.$valores['genero'].'</option>';
+} */
 
 ?>
